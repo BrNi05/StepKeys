3ab0f0a8fdd001418dad28f956a9ea583b84be1d
BrNi05 <szocsbarnabas8@gmail.com>
2026-01-18 18:36:30 +0100

    Add Arduino code

---
+++ arduino/code/stepkeys.ino
@@ -0,0 +1,46 @@
+// The first digital pin a pedal is connected to
+constexpr int FIRST_PIN = 2;
+
+// The number of pedals connected
+// So the last pin used will be FIRST_PIN + NUM_PEDALS - 1
+constexpr int NUM_PEDALS = 5;
+
+uint8_t pedalState[NUM_PEDALS];
+uint8_t prevPedalState[NUM_PEDALS];
+
+void setup() {
+  // Baud rate: 115200
+  Serial.begin(115200);
+
+  for (uint8_t i = 0; i < NUM_PEDALS; i++) {
+    pinMode(FIRST_PIN + i, INPUT);
+    pedalState[i] = LOW;
+    prevPedalState[i] = LOW;
+  }
+}
+
+void loop() {
+  for (uint8_t i = 0; i < NUM_PEDALS; i++) {
+    pedalState[i] = digitalRead(FIRST_PIN + i);
+
+    // Transition: LOW -> HIGH (PRESS)
+    // MSB: 1
+    if (pedalState[i] == HIGH && prevPedalState[i] == LOW) {
+      Serial.write(i | 0x80);
+    }
+
+    // Transition: HIGH -> LOW (RELEASE)
+    // MSB: 0
+    if (pedalState[i] == LOW && prevPedalState[i] == HIGH) {
+      Serial.write(i);
+    }
+
+    prevPedalState[i] = pedalState[i];
+  }
+}
+
+// The protocol:
+// Arduino sends a single byte when a pedal is pressed or released 
+// The byte is used like: first bit (MSB) for the event type (1=press, 0=release), following bits for pedal ID (in decimal: 0 to NUM_PEDALS-1)
+// Example: 00000010 = Pedal 2 released
+//          10000001 = Pedal 1 pressed
