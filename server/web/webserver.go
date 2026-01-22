414eec4a044e3e9a3c6417106e3d09cdfba5499a
BrNi05 <szocsbarnabas8@gmail.com>
2026-01-22 22:32:06 +0100

    Add webGUI handler

---
+++ server/web/webserver.go
@@ -0,0 +1,24 @@
+package web
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	Config "stepkeys/server/config"
+)
+
+// Serve the webGUI
+func StartGUI() {
+	webDir := filepath.Join(filepath.Dir(Config.GetExecPath()), "webgui")
+
+	fs := http.FileServer(http.Dir(webDir))
+	http.Handle("/", fs)
+
+	addr := fmt.Sprintf(":%d", Config.GetWebPort())
+	log.Println("Starting webGUI on port", addr, "serving folder:", webDir)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Println("Failed to start webGUI:", err)
+	}
+}
