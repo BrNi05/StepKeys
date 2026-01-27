60838a55f03adc67cf6743e341a2556fb75e8576
BrNi05 <szocsbarnabas8@gmail.com>
2026-01-27 12:54:36 +0100

    Resolve path aliases

--- gui/vite.config.ts
+++ gui/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import UnoCSS from 'unocss/vite';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [vue(), UnoCSS()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:18000',
